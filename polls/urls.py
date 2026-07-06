90e3c707fb9f98d9c0d948bf3d5b09549e491204
robbiesollie <robbiesollie@gmail.com>
2026-07-06 10:26:11 -0700

    Added very incredibly basic views

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
+    #   /polls/
     path("", views.index, name="index"),
+    #   /polls/#
+    path("<int:question_id>/", views.detail, name="detail"),
+    #   /polls/#/results
+    path("<int:question_id>/results/", views.results, name="results"),
+    #   /polls/#/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
