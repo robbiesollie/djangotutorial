7923da90bd2dbd4d2d8abaf70498d2a4ec97dc40
robbiesollie <robbiesollie@gmail.com>
2026-07-13 16:38:29 -0700

    Modify Base Django Templates

--- polls/models.py
+++ polls/models.py
@@ -2,13 +2,23 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+
+
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published Recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
